ca651d641d887bc49b68567b3b48d8224fc64d3d
Will Neal <willwneal@gmail.com>
2022-07-11 17:10:04 -0400

    Built basic server with handlebars viewengine

---
+++ server.js
@@ -0,0 +1,32 @@
+//Standard Imports
+const path = require('path');
+
+//server import
+const express = require('express');
+
+//handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
+//Project imports
+
+//PORT
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(session(sess));
+
+//View-engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('<h1>HELLO, WORLD! WELCOME TO THE HOMEPAGE</h1>')
+})
+
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`))

db34bd05fc46e4cbc0afe84275393bc5d50154d1
Will Neal <willwneal@gmail.com>
2022-07-22 18:19:36 -0400

    Adding inputs and js logic for adding ingredients

---
+++ public/js/admin.js
@@ -0,0 +1,13 @@
+//Button to add another ingredient
+const addButton = document.querySelector("#add-button")
+//grab ingredient input field div 
+const ingredientInput = document. querySelector("#ingredient-input")
+
+let ingredientNum = 2
+const addIngredient = () => {
+    console.log("Add button clicked")
+    const label = document.create 
+}
+
+
+addButton.addEventListener('click', addIngredient)

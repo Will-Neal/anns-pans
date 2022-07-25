7739348d56d6fafaab0b0026bc3608389837452d
Will Neal <willwneal@gmail.com>
2022-07-25 17:35:09 -0400

    Logic to combine ingredients/quantities in recipe

--- server.js
+++ server.js
@@ -55,13 +55,20 @@ app.get('/admin', (req, res) => {
 
 app.get('/recipe/:id', async (req, res) => {
     const recipe = await db.collection('recipe').findOne({ _id: new ObjectID(req.params.id)})
-    // console.log(recipe)
-    res.render('recipe', {recipe})
+    const ingredients = recipe.ingredients
+    const quantity = recipe.quantity
+    const ingredientsArray = []
+    for (i=0; i < ingredients.length; i++) {
+        let ingredientAndQuantity = [ingredients[i], quantity[i]]
+        ingredientsArray.push(ingredientAndQuantity)
+    }
+    // console.log(ingredientsArray)
+    res.render('recipe', {recipe, ingredientsArray})
 })
 
 app.route('/recipe').get(async (req, res) => {
     const recipes = await db.collection('recipe').find({}).toArray()
-    // console.log(recipes)
+    console.log(recipes)
     for (const recipe of recipes) {
         console.log(recipe.title)
         console.log(recipe.style)
